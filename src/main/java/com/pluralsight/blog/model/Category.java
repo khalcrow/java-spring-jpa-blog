18e0b65aecaa92a0ad7166e681d616275ded88b3
Keith Halcrow <keith.halcrow@digital.homeoffice.gov.uk>
2020-05-16 18:44:44 +0100

    update

--- src/main/java/com/pluralsight/blog/model/Category.java
+++ src/main/java/com/pluralsight/blog/model/Category.java
@@ -1,36 +1,28 @@
 package com.pluralsight.blog.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
-
+@Getter
+@Setter
+@Entity
 public class Category {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
+    @OneToMany
+    private List<Post> posts;
+
     public Category() {
         super();
+        posts = new ArrayList<>();
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public List<Post> getPosts() {
-        return null;
-    }
-
-    public void addPost(Post post) {
-        return;
-    }
 }
--- src/main/java/com/pluralsight/blog/model/Post.java
+++ src/main/java/com/pluralsight/blog/model/Post.java
@@ -1,24 +1,35 @@
 package com.pluralsight.blog.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @Entity
+@Getter
+@Setter
 public class Post {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String title;
     private String lead;
+
     @Column(length=1000000)
     @Lob
     private String body;
     private String author;
     @Temporal(TemporalType.DATE)
     private Date date;
+
+    @ManyToOne
+    private Category category;
+
 
     public Post() {
         super();
@@ -34,56 +45,9 @@ public class Post {
         this.date = date;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getLead() {
-        return lead;
-    }
-
-    public void setLead(String lead) {
-        this.lead = lead;
-    }
-
-    public String getBody() {
-        return body;
-    }
-
-    public void setBody(String body) {
-        this.body = body;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
     public String getDateStr() {
         DateFormat outputFormatter = new SimpleDateFormat("MM/dd/yyyy");
         return outputFormatter.format(this.date);
     }
 
-    public void setDate(Date date) {
-        this.date = date;
-    }
 }
